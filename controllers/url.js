26f12c6fd84c28a03a8da201db43394d455605cf
Bikram Mondal <codesnippets45@gmail.com>
2025-12-29 22:18:29 +0530

    Frontend done

--- controllers/url.js
+++ controllers/url.js
@@ -3,7 +3,7 @@ const URL = require('../models/url')
 
 async function generateNewShortURL(req, res) {
     const body = req.body
-    if(!body.originalUrl) return res.status(400).json({ error: "URL is required"})
+    if (!body.originalUrl) return res.status(400).json({ error: "URL is required" })
 
     const shortId = nanoid(7)
     try {
@@ -20,7 +20,7 @@ async function generateNewShortURL(req, res) {
     }
 }
 
-async function redirectURL (req, res) {
+async function redirectURL(req, res) {
     const shortId = req.params.id
     try {
         const urlEntry = await URL.findOneAndUpdate(
@@ -38,12 +38,12 @@ async function redirectURL (req, res) {
     }
 }
 
-async function uiRenderFunction (req, res) {
+async function uiRenderFunction(req, res) {
     res.render('home')
 }
 
 module.exports = {
     generateNewShortURL,
     redirectURL,
-
+    uiRenderFunction
 }
